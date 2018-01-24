85173f0d6df9c62c6b761f4fbdb64515368f57ba
barton-white <barton.w13@gmail.com>
2018-01-23 19:08:33 -0800

    Figured out how to get calls to giphy to work

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var gifApp = angular.module('gifApp', ['ngRoute', 'ngResource']);
 
 //routing
-gifApp.config(function($routeProvider){
+gifApp.config(function($routeProvider, $sceDelegateProvider){
 	$routeProvider
 		.when('/',{
 			templateUrl: 'pages/main.html',
@@ -11,4 +11,25 @@ gifApp.config(function($routeProvider){
 			templateUrl: 'pages/pagetwo.html',
 			controller: 'mainCtrl'
 		});
+        
+    $sceDelegateProvider.resourceUrlWhitelist([
+        'self',
+        'http://api.giphy.com/v1/gifs/**'
+      ]);
 });
+
+//controllers
+gifApp.controller('mainCtrl', ['$scope', '$log', '$resource', function($scope, $log, $resource){
+    
+    $scope.randomGif = $resource('http://api.giphy.com/v1/gifs/random');
+    //, {callback: "JSON_CALLBACK"}, { get: {method: "JSONP"}});
+    
+    //local test 
+    //$scope.randomGif = $resource('random.json');
+    
+    $scope.gifResult = $scope.randomGif.get({api_key: 'U7FFjpMhs2ewS7hvDwcBmf3gS1cSgvbq'});
+    
+    $log.log($scope.gifResult);
+    //api_key: 'U7FFjpMhs2ewS7hvDwcBmf3gS1cSgvbq'
+    
+}]);
