55f0c9a114bc4b0ec9db48fba5a7c9bddf236050
barton-white <barton.w13@gmail.com>
2018-01-26 14:41:17 -0800

    Started search function

--- js/app.js
+++ js/app.js
@@ -7,6 +7,14 @@ gifApp.config(function($routeProvider, $sceDelegateProvider){
 			templateUrl: 'pages/main.html',
 			controller: 'mainCtrl'
 		})
+        .when('/search',{
+			templateUrl: 'pages/search.html',
+			controller: 'resultCtrl'
+		})
+        .when('/search/:search-term',{
+			templateUrl: 'pages/search.html',
+			controller: 'resultCtrl'
+		})
         .when('/page-two',{
 			templateUrl: 'pages/pagetwo.html',
 			controller: 'mainCtrl'
@@ -18,7 +26,13 @@ gifApp.config(function($routeProvider, $sceDelegateProvider){
       ]);
 });
 
+//services
+gifApp.service('searchData', function(){
+    this.searchTerm = '';
+});
+
 //controllers
+
 gifApp.controller('mainCtrl', ['$scope', '$log', '$resource', function($scope, $log, $resource){
     
     $scope.randomGif = $resource('http://api.giphy.com/v1/gifs/random');
@@ -28,6 +42,37 @@ gifApp.controller('mainCtrl', ['$scope', '$log', '$resource', function($scope, $
     //$scope.randomGif = $resource('random.json');
     
     $scope.gifResult = $scope.randomGif.get({api_key: 'U7FFjpMhs2ewS7hvDwcBmf3gS1cSgvbq'});
+    
+    $log.log($scope.gifResult);
+    //api_key: 'U7FFjpMhs2ewS7hvDwcBmf3gS1cSgvbq'
+    
+}]);
+
+gifApp.controller('searchCtrl', ['$scope', '$log', '$location', 'searchData', function($scope, $log, $location, searchData){
+    
+    $scope.searchInput = "";
+    $log.log("test");
+    $scope.$watch('searchInput', function(){
+            searchData.searchTerm = $scope.searchInput;
+            $log.log($scope.searchInput);
+    });
+    
+    $scope.submit = function(){
+        $location.path("/search/" + searchData.searchTerm);
+        //$log.log(searchData.searchTerm);
+    };
+    
+}]);
+                                 
+gifApp.controller('resultCtrl', ['$scope', '$log', '$resource', '$location', function($scope, $log, $resource, $location){
+    
+    $scope.randomGif = $resource('http://api.giphy.com/v1/gifs/search');
+    //, {callback: "JSON_CALLBACK"}, { get: {method: "JSONP"}});
+    
+    //local test 
+    //$scope.randomGif = $resource('random.json');
+    
+    $scope.gifResult = $scope.randomGif.get({api_key: 'U7FFjpMhs2ewS7hvDwcBmf3gS1cSgvbq', q: search-term});
     
     $log.log($scope.gifResult);
     //api_key: 'U7FFjpMhs2ewS7hvDwcBmf3gS1cSgvbq'
